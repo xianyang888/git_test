05cb5246f638f28f2bcd4d8039ac5b711772fd23
xianyang <2228191342@qq.com>
2025-01-16 18:21:27 +0800

    second save

--- test.c
+++ test.c
@@ -4,6 +4,11 @@ int main() {
     printf("Hello, world!\n");
     int n;
     scanf("%d", &n);    
-    printf("You entered: %d\n", n);        
+    printf("You entered: %d\n", n);    
+    if(n > 10) {    
+        printf("n is greater than 10\n");    
+    } else {    
+        printf("n is less than or equal to 10\n");        
+    }    
     return 0;
 }
